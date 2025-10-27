8e5c589aee3074badc0b3899ee5dabb6394bdf53
taimourz <taimour.rkt@gmail.com>
2025-10-27 20:03:15 +0500

    create reading resources

--- src/index.js
+++ src/index.js
@@ -8,6 +8,54 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(404).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.status(201).send(user)
+
+    }).catch(error => {
+        res.status(404).send(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(404).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        
+        if(!task){
+            return res.status(404).send()
+        }
+
+        res.status(201).send(task)
+
+    }).catch(error => {
+        res.status(404).send(error)
+    })
+})
+
 app.post('/tasks', (req, res) =>{
 
     const task = new Task(req.body)
